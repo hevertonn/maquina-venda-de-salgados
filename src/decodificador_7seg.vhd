4236f1bf21bc3303843f7d63b80bf569f0e4e883
Heverton <hb.hevertonborges@gmail.com>
2026-07-05 13:22:12 -0300

    Fix display decoder

--- src/decodificador_7seg.vhd
+++ src/decodificador_7seg.vhd
@@ -15,40 +15,40 @@ begin
   p_decodificador_7seg : process (digito) is
   begin
 
-    -- Ordem dos bits: abcdefg. Padrao ativo em nivel baixo.
+    -- Ordem dos bits: gfedcba. Padrao ativo em nivel baixo.
     case digito is
 
       when "0000" =>
 
-        segmentos <= "0000001"; -- 0
+        segmentos <= "1000000"; -- 0
 
       when "0001" =>
 
-        segmentos <= "1001111"; -- 1
+        segmentos <= "1111001"; -- 1
 
       when "0010" =>
 
-        segmentos <= "0010010"; -- 2
+        segmentos <= "0100100"; -- 2
 
       when "0011" =>
 
-        segmentos <= "0000110"; -- 3
+        segmentos <= "0110000"; -- 3
 
       when "0100" =>
 
-        segmentos <= "1001100"; -- 4
+        segmentos <= "0011001"; -- 4
 
       when "0101" =>
 
-        segmentos <= "0100100"; -- 5
+        segmentos <= "0010010"; -- 5
 
       when "0110" =>
 
-        segmentos <= "0100000"; -- 6
+        segmentos <= "0000010"; -- 6
 
       when "0111" =>
 
-        segmentos <= "0001111"; -- 7
+        segmentos <= "1111000"; -- 7
 
       when "1000" =>
 
@@ -56,7 +56,7 @@ begin
 
       when "1001" =>
 
-        segmentos <= "0000100"; -- 9
+        segmentos <= "0010000"; -- 9
 
       when others =>
 
